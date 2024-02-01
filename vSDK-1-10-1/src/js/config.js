ea856e1a0b40b5238b28f46402341a561dc70fc5
caleb-loganzoom <144465929+caleb-loganzoom@users.noreply.github.com>
2024-02-01 15:19:40 -0600

    Add files via upload

---
+++ vSDK-1-10-1/src/js/config.js
@@ -0,0 +1,9 @@
+export default {
+    sdkKey: '3EZYnRL4m2sgiEocSvTv5Hwpn0PQz5K3VB5K',
+    sdkSecret: 'DQdJkiamUbHcCTsPaQejf9tqYuyi2XDhx1tj',
+    topic: 'test123',
+    password: '',
+    name: 'Test123',
+    sessionKey: '',
+    user_identity: ''
+};
---
+++ vSDK-1-10-1/src/js/meeting/session/simple-state.js
@@ -0,0 +1,28 @@
+/**
+ * A simple class to contain, read, and modify our app's state
+ * 
+ * If familiar with redux, it is the recommend approach for state management
+ */
+class SimpleState {
+  constructor() {
+    this.reset();
+  }
+
+  /**
+   * Resets state to default values
+   */
+  reset() {
+    this.selfId = -1;
+    this.participants = [];
+    this.audioEncode = false;
+    this.audioDecode = false;
+    this.isStartedAudio = false;
+  }
+
+  resetParticipantId() {
+    this.participants = [];
+  }
+}
+
+// Provide global state
+export default new SimpleState();
