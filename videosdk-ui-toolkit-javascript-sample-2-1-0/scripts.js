9750840bab98c68cb813dc2623f24e5467dcfed8
caleb-loganzoom <144465929+caleb-loganzoom@users.noreply.github.com>
2025-01-28 12:38:55 -0600

    Update scripts.js

--- videosdk-ui-toolkit-javascript-sample-2-1-0/scripts.js
+++ videosdk-ui-toolkit-javascript-sample-2-1-0/scripts.js
@@ -3,12 +3,12 @@ import uitoolkit from './@zoom/videosdk-ui-toolkit/index.js'
 var sessionContainer = document.getElementById('sessionContainer')
 var authEndpoint = ''
 var config = {
-    videoSDKJWT: '',
+    videoSDKJWT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiM0VaWW5STDRtMnNnaUVvY1N2VHY1SHdwbjBQUXo1SzNWQjVLIiwicm9sZV90eXBlIjoxLCJ0cGMiOiJ0ZXN0IiwiaWF0IjoxNzM4MDg5NTA2LCJleHAiOjE3MzgwOTMxMDZ9.flKv5siDkL8bR2nNLQI1Cpmazfvvr8uuGZBYAnaZjuo',
     sessionName: 'test',
     userName: 'JavaScript',
     sessionPasscode: '123',
-    features: ['preview', 'video', 'audio', 'settings', 'users', 'chat', 'share'],
-    options: { init: {}, audio: {}, video: {}, share: {}},
+    features: ['video', 'audio', 'settings', 'users', 'chat', 'share'],
+    options: { init: {enforceVirtualBackground: true}, audio: {}, video: {virtualBackground: { cropped: true, imageUrl:'https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop'}}, share: {}},
     virtualBackground: {
        allowVirtualBackground: true,
        allowVirtualBackgroundUpload: true,
